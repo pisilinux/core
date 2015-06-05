4407e9fcefeded6280db674e35f85d56d4203cc6
Ertuğrul Erata <ertugrulerata@gmail.com>
2015-06-06 00:43:07 +0300

    procps and tar v.bump

--- system/base/procps/actions.py
+++ system/base/procps/actions.py
@@ -8,13 +8,27 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+def setup():
+    pisitools.dosed("watch.c", "ncursesw/ncurses.h", "ncurses.h")
+    
+    autotools.configure("--prefix=/usr \
+                         --exec-prefix=/ \
+                         --disable-static \
+                         --sysconfdir=/etc \
+                         --libdir=/usr/lib \
+                         --bindir=/usr/bin \
+                         --sbindir=/usr/bin \
+                         --enable-watch8bit \
+                         --without-systemd")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+
 def build():
-    autotools.make('CC=%s CPPFLAGS="%s" CFLAGS="%s" LDFLAGS="%s" lib64=lib' % \
-                   (get.CC(), get.CXXFLAGS(), get.CFLAGS(), get.LDFLAGS()))
+    autotools.make()
 
 def install():
     autotools.rawInstall('ln_f="ln -sf" ldconfig="true" lib64=lib DESTDIR=%s SKIP="/bin/kill /usr/share/man/man1/kill.1"' % get.installDIR())
 
     pisitools.dosym("libproc-%s.so" % get.srcVERSION(), "/lib/libproc.so")
 
-    pisitools.dodoc("BUGS", "NEWS", "TODO", "ps/HACKING")
+    pisitools.dodoc("NEWS", "ps/HACKING")
