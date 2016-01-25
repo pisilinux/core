01311c63b5214cad074bbbced8bede9feb82b593
Ertuğrul Erata <ertugrulerata@gmail.com>
2016-01-25 22:31:07 +0200

    gperftools update

--- system/base/gperftools/actions.py
+++ system/base/gperftools/actions.py
@@ -10,10 +10,6 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    # fix for glibc 2.16
-    #pisitools.dosed("src/base/linuxthreads.cc", "siginfo_t", "siginfo")
-    shelltools.chmod("autogen.sh", 0755)
-    shelltools.system("./autogen.sh")
     autotools.configure("--disable-static \
                          --disable-dependency-tracking \
                          --enable-fast-install \
@@ -23,6 +19,8 @@ def setup():
     # Disable rpath
     pisitools.dosed("libtool", "^hardcode_libdir_flag_spec=.*", "hardcode_libdir_flag_spec=\"\"")
     pisitools.dosed("libtool", "^runpath_var=LD_RUN_PATH", "runpath_var=DIE_RPATH_DIE")
+    
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
     autotools.make("noinst_PROGRAMS=' '")
