89afc1291c41bd46bf1b39ae931f6479d65ed097
Rmys <muscnsl@hotmail.com>
2018-04-22 03:42:56 +0300

    rhash-1.3.6:Release Bump Cmake icin

---
+++ system/devel/rhash/actions.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+
+def setup():
+    shelltools.export("CFLAGS","%s -fPIC -DPIC" % get.CFLAGS())
+    shelltools.export("LDFLAGS","%s -fPIC" % get.LDFLAGS())
+    shelltools.system("./configure --prefix=/usr --sysconfdir=/etc")
+    
+    
+def install():
+    autotools.make("PREFIX=/usr DESTDIR=%s build-shared install-lib-shared" % get.installDIR())
+    autotools.make("PREFIX=/usr DESTDIR=%s -C librhash install-so-link install-headers" % get.installDIR())
+    autotools.rawInstall("PREFIX=/usr DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("ChangeLog", "INSTALL*", "README", "COPYING")
+
