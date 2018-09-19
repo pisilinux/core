846151e2ab5b8ff621413be3f7666626adf856a0
Rmys <muscnsl@gmail.com>
2018-09-19 11:57:11 +0300

    docbook-xml

--- office/docbook/docbook-xml/actions.py
+++ office/docbook/docbook-xml/actions.py
@@ -41,5 +41,13 @@ def install():
         pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "*.mod")
         pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "docbook.cat")
         pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s/ent" % version, "ent/*.ent")
+        
+    for version in ["5.0"]:
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "docbook-5.0/dtd/*.dtd")
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "docbook-5.0/rng/*.rng")
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "docbook-5.0/rng/*.rnc")
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "docbook-5.0/xsd/*.xsd")
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "docbook-5.0/sch/*.sch")
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "docbook-5.0/catalog.xml")
     
     pisitools.dodoc("ChangeLog", "README")
