c5cf773c78023a6448d247384940199e48d1d6b1
Rmys <muscnsl@hotmail.com>
2018-09-19 11:51:08 +0300

    docbook-xml:rebuild

--- office/docbook/docbook-xml/actions.py
+++ office/docbook/docbook-xml/actions.py
@@ -5,15 +5,41 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 WorkDir="."
 
+
 def install():
-    for version in ["4.1.2", "4.2", "4.3", "4.4", "4.5", "5.0"]:
+    for version in ["4.1.2"]:
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "4.1.2/*.dtd")
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "4.1.2/*.mod")
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "4.1.2/docbook.cat")
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s/ent" % version, "4.1.2/ent/*.ent")
+        
+    for version in ["4.2"]:
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "4.2/*.dtd")
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "4.2/*.mod")
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "4.2/docbook.cat")
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s/ent" % version, "4.2/ent/*.ent")
+        
+    for version in ["4.3"]:
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "4.3/*.dtd")
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "4.3/*.mod")
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "4.3/docbook.cat")
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s/ent" % version, "4.3/ent/*.ent")
+        
+    for version in ["4.4"]:
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "4.4/*.dtd")
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "4.4/*.mod")
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "4.4/docbook.cat")
+        pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s/ent" % version, "4.4/ent/*.ent")
+        
+    for version in ["4.5"]:
         pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "*.dtd")
         pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "*.mod")
         pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s" % version, "docbook.cat")
         pisitools.insinto("/usr/share/xml/docbook/xml-dtd-%s/ent" % version, "ent/*.ent")
-
+    
     pisitools.dodoc("ChangeLog", "README")
