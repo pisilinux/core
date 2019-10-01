4c08de87c7807e49e76d9e9c71faebb9d7ba539a
İdris Kalp <idriskalp@gmail.com>
2019-10-01 21:37:37 +0300

    python ver bump

--- system/base/python/actions.py
+++ system/base/python/actions.py
@@ -61,5 +61,6 @@ def install():
     pisitools.dosym("/usr/bin/python%s" % PythonVersion, "/usr/bin/python")
     pisitools.dosym("/usr/bin/python%s-config" % PythonVersion, "/usr/bin/python-config")
     pisitools.dosym("/usr/lib/python%s/pdb.py" % PythonVersion, "/usr/bin/pdb")
+    #pisitools.domove("/usr/lib/python2.7/lib-dynload/bz2_failed.so", "/usr/lib/python2.7/lib-dynload", "bz2.so")
 
     pisitools.dodoc("LICENSE", "README")
