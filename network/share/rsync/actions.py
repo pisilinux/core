0d8b8493e8ad27ad70e0e2c89e9a05db7603809d
İdris Kalp <idriskalp@gmail.com>
2019-10-01 20:47:43 +0300

    rsync move to core

---
+++ network/share/rsync/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+
+def setup():
+    autotools.configure("--without-included-zlib \
+                         --with-included-popt=no \
+                         --disable-debug")
+
+def build():
+    autotools.make()
+
+#def check():
+#    autotools.make("test")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("NEWS","README","TODO")
