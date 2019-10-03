f82e181d8d9589065435802dacb1454f6543ec78
İdris Kalp <idriskalp@gmail.com>
2019-10-03 17:39:15 +0300

    liblogging ver bump

--- system/base/liblogging/actions.py
+++ system/base/liblogging/actions.py
@@ -8,6 +8,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
+    autotools.autoreconf("-vfi")
     autotools.configure("--disable-static \
                          --disable-rfc3195 \
                          --disable-journal")
