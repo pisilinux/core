a3319784f4036a1098d9ac9d11ab70936f62af57
İdris Kalp <idriskalp@gmail.com>
2019-10-03 17:27:05 +0300

    xorg-proto ver bump

--- system/devel/xorg-proto/actions.py
+++ system/devel/xorg-proto/actions.py
@@ -12,24 +12,30 @@ from pisi.actionsapi import get
 def setup():
     shelltools.system("sed -i 's|datadir|libdir|g' meson.build")
     
-    shelltools.makedirs("build")
-    shelltools.cd("build")
-    shelltools.system("meson .. --prefix=/usr --libdir=lib -Dlegacy=true")
+    #shelltools.makedirs("build")
+    #shelltools.cd("build")
+    #shelltools.system("meson .. --prefix=/usr --libdir=lib -Dlegacy=true")
+    autotools.configure("--enable-legacy")
     
 def build():
-    shelltools.cd("build")
-    shelltools.system("ninja")
+    #shelltools.cd("build")
+    #shelltools.system("ninja")
+    autotools.make()
     
 def install():
-    shelltools.cd("build")
-    shelltools.system("DESTDIR=%s ninja install" % get.installDIR())
+    #shelltools.cd("build")
+    #shelltools.system("DESTDIR=%s ninja install" % get.installDIR())
     
-    shelltools.cd("..")
+    #shelltools.cd("..")
+    autotools.install()
     pisitools.dodoc("README.md", "COPYING*", "AUTHORS")
+    shelltools.system("mkdir -p %s/usr/lib/pkgconfig" %get.installDIR())
+    shelltools.system("mv %s/usr/share/pkgconfig/* %s/usr/lib/pkgconfig" %(get.installDIR(), get.installDIR()))
+    pisitools.removeDir("/usr/share/pkgconfig")
     
-    pisitools.remove("/usr/include/X11/extensions/apple*")
-    pisitools.remove("/usr/include/X11/extensions/windows*")
-    pisitools.remove("/usr/share/doc/xorg-proto/COPYING-windowswmproto")
-    pisitools.remove("/usr/share/doc/xorg-proto/COPYING-applewmproto")
-    pisitools.remove("/usr/lib/pkgconfig/applewmproto.pc")
-    pisitools.remove("/usr/lib/pkgconfig/windowswmproto.pc")
+    #pisitools.remove("/usr/include/X11/extensions/apple*")
+    #pisitools.remove("/usr/include/X11/extensions/windows*")
+    #pisitools.remove("/usr/share/doc/xorg-proto/COPYING-windowswmproto")
+    #pisitools.remove("/usr/share/doc/xorg-proto/COPYING-applewmproto")
+    #pisitools.remove("/usr/lib/pkgconfig/applewmproto.pc")
+    #pisitools.remove("/usr/lib/pkgconfig/windowswmproto.pc")
