192a457ef38abc45f21b4ebaa681e8e6d28d8e6c
idriskalp <idriskalp@gmail.com>
2019-12-27 14:10:52 +0300

    leveldb revert to 1.18

--- system/base/leveldb/actions.py
+++ system/base/leveldb/actions.py
@@ -6,27 +6,20 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import cmaketools
-from pisi.actionsapi import get
-
-def setup():
-    cmaketools.configure("-DBUILD_SHARED_LIBS=ON \
-		                  -DCMAKE_INSTALL_LIBDIR=lib")
 
 def build():
-    #shelltools.chmod("build_detect_platform", 0755)
-    cmaketools.make()
+    shelltools.chmod("build_detect_platform", 0755)
+    autotools.make()
 
-def check():
-    cmaketools.make("test")
+#def check():
+    #autotools.make("check")
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    #pisitools.dolib_so("libleveldb.so.1.18")
-    #pisitools.dosym("libleveldb.so.1.18", "/usr/lib/libleveldb.so.1")
-    #pisitools.dosym("libleveldb.so.1.18", "/usr/lib/libleveldb.so")
+    pisitools.dolib_so("libleveldb.so.1.18")
+    pisitools.dosym("libleveldb.so.1.18", "/usr/lib/libleveldb.so.1")
+    pisitools.dosym("libleveldb.so.1.18", "/usr/lib/libleveldb.so")
 
-    #pisitools.insinto("/usr/include", "include/*")
-    #pisitools.insinto("/usr/include", "helpers/memenv/memenv.h")
+    pisitools.insinto("/usr/include", "include/*")
+    pisitools.insinto("/usr/include", "helpers/memenv/memenv.h")
 
-    pisitools.dodoc("README.md", "LICENSE", "NEWS", "AUTHORS")
+    pisitools.dodoc("README", "LICENSE", "NEWS", "AUTHORS")
