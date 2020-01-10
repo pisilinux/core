76a37adac6c8a782756ef318986289b56c7e3e0a
Safa Arıman <safa@ariman.gen.tr>
2020-01-10 04:30:17 +0300

    Pisi version upgrade. Removed some unnecessary files from kernel package.

--- system/base/pisi/actions.py
+++ system/base/pisi/actions.py
@@ -8,27 +8,23 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import pythonmodules
 
+
 def build():
-    pisitools.dosed("pisi/__init__.py", "2.4", "2.6.1")
-    pisitools.dosed("pisi/db/lazydb.py", "2.4", "2.6.1")
     pythonmodules.compile()
 
+
 def install():
-    # Install into /usr/lib/pardus so we can protect ourself from python updates
     pythonmodules.install("--install-lib=/usr/lib/pisilinux")
 
     pisitools.dosym("pisi-cli", "/usr/bin/pisi")
 
     shelltools.touch("LOCK")
-    shelltools.chmod("LOCK", 0666)
+    shelltools.chmod("LOCK", 0o666)
     pisitools.dodir("/run/lock/files.ldb")
     pisitools.insinto("/run/lock/files.ldb", "LOCK")
     pisitools.dodir("/var/lib/pisi/info/files.ldb")
     pisitools.dosym("/run/lock/files.ldb/LOCK", "/var/lib/pisi/info/files.ldb/LOCK")
 
-
-    pisitools.insinto("/etc/pisi", "pisi.conf-%s" % get.ARCH(), "pisi.conf")
-
-    # we need it teporary
+    # we need it temporary
     pisitools.dodir("/usr/lib/pardus")
     pisitools.dosym("/usr/lib/pisilinux/pisi", "/usr/lib/pardus/pisi")
