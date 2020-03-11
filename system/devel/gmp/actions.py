d346741d4404d57e14146f2b3a252a349834fb4b
blue-devil <bluedevil@sctzine.com>
2020-03-12 01:24:50 +0300

    gmp:ver bump

--- system/devel/gmp/actions.py
+++ system/devel/gmp/actions.py
@@ -4,9 +4,9 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 
@@ -30,4 +30,4 @@ def install():
     if get.buildTYPE() == "emul32": return
 
     pisitools.doinfo("doc/*info*")
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "NEWS", "README")
+    pisitools.dodoc("COPYING*", "README")
