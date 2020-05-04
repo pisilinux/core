990d13845768393bb8abb58fa77642e780b41676
Rmys <muscnsl@gmail.com>
2020-05-04 13:47:29 +0300

    biosdevname-0.7.3 ver. bump

--- system/base/biosdevname/actions.py
+++ system/base/biosdevname/actions.py
@@ -4,10 +4,12 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
 def setup():
+    shelltools.system("sh ./autogen.sh")
     autotools.configure()
 
 def build():
