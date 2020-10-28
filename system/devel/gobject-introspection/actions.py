92de41d638199b46332b70542f13dbe6dd6b80b3
Rmys <muscnsl@gmail.com>
2020-10-28 22:15:53 +0200

    gobject-introspection

--- system/devel/gobject-introspection/actions.py
+++ system/devel/gobject-introspection/actions.py
@@ -4,38 +4,19 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import mesontools
+from pisi.actionsapi import pisitools
 
 
 def setup():
-    #autotools.autoreconf("-fiv")
-    #autotools.configure("--disable-static \
-                         #--disable-doctool")
-                         
-    options = "  --libdir=/usr/lib \
-				 --bindir=/usr/bin \
-                 --sbindir=/usr/sbin \
-                 --datadir=/usr/share \
-                 --prefix=/usr"
-                 
-    shelltools.makedirs("build")
-    shelltools.cd("build")
-    shelltools.system("meson .. %s" % options)
-    
-    #pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+    mesontools.configure()
 
 def build():
-    #autotools.make()
-    shelltools.cd("build")
-    shelltools.system("ninja")
-
+    mesontools.build()
+    
 def install():
-    #autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    shelltools.cd("build")
-    shelltools.system("DESTDIR=%s ninja install" % get.installDIR())
+    mesontools.install()
 
     
 
