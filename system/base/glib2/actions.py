3c69664683b0c6deef773f3b25bfacfdb756dab4
Rmys <muscnsl@gmail.com>
2020-10-31 00:16:55 +0200

    glib2

--- system/base/glib2/actions.py
+++ system/base/glib2/actions.py
@@ -6,10 +6,9 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
+from pisi.actionsapi import mesontools
 from pisi.actionsapi import shelltools
-
-builddir = "build32" if get.buildTYPE() == 'emul32' else "build"
+from pisi.actionsapi import get
 
 def setup():
     options = "-Dselinux=disabled \
@@ -18,49 +17,36 @@ def setup():
 
 
     if get.buildTYPE() == "_emul32":
-        options += " --libdir=/_emul32/lib32 \
-                     --bindir=/_emul32/bin \
-                     --sbindir=/_emul32/sbin \
-                     --prefix=/_emul32 \
-                     -Dlibmount=disabled"
         shelltools.export("CC", "%s -m32" % get.CC())
         shelltools.export("CXX", "%s -m32" % get.CXX())
         shelltools.export("PKG_CONFIG_PATH", "/usr/lib32/pkgconfig")
+        options += " --libdir=/usr/lib32 \
+                     --bindir=/usr/emul32 \
+                     --sbindir=/usr/emul32 \
+                     --datadir=/usr/_emul32 \
+                     --mandir=/usr/emul32 \
+                     --localedir=/usr/emul32/locale \
+                     -Dlibmount=disabled"
+        
         shelltools.system("patch -p1 < multilib.diff")
-    else :
-        options += " --libdir=/usr/lib \
-                     --bindir=/usr/bin \
-                     --sbindir=/usr/sbin \
-                     --datadir=/usr/share \
-                     --prefix=/usr"
+    mesontools.configure(options)
 
-    #autotools.autoreconf("-vif")
-    shelltools.makedirs("%s" %builddir)
-    shelltools.cd("%s" %builddir)
-    shelltools.system("meson .. %s" % options)
-
-    #pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
-    #autotools.make("-j1")
-    shelltools.cd("%s" %builddir)
-    shelltools.system("ninja")
+    mesontools.build()
 
 def install():
-    shelltools.cd("%s" %builddir)
-    #autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    shelltools.system("DESTDIR=%s ninja install" % get.installDIR())
+    mesontools.install()
 
     if get.buildTYPE() == "_emul32":
-        pisitools.domove("/_emul32/bin/gio-querymodules", "/usr/bin/32/")
-        pisitools.domove("/_emul32/lib32", "/usr")
-        pisitools.removeDir("/_emul32")
+        pisitools.domove("/usr/emul32//gio-querymodules", "/usr/bin/32/")
+        pisitools.removeDir("usr/emul32")
+        pisitools.removeDir("usr/_emul32")
+        pisitools.removeDir("/usr/share/gdb")
         for f in shelltools.ls("%s/usr/lib32/pkgconfig" % get.installDIR()):
-            pisitools.dosed("%s/usr/lib32/pkgconfig/%s" % (get.installDIR(), f), "_emul32", "usr") 
+            pisitools.dosed("%s/usr/lib32/pkgconfig/%s" % (get.installDIR(), f), "_emul32", "share")
+            pisitools.dosed("%s/usr/lib32/pkgconfig/%s" % (get.installDIR(), f), "emul32", "bin")
         return
 
     
-    if get.buildTYPE() != "_emul32":
-        pisitools.removeDir("/usr/share/gdb")
-        shelltools.cd("..")
-        pisitools.dodoc("AUTHORS", "COPYING", "README*")
+    pisitools.dodoc("AUTHORS", "COPYING", "README*")
