e972ed878bbc3f6fb2f3a14f6f234289bbac67bd
Rmys <muscnsl@gmail.com>
2021-03-13 15:41:50 +0300

    libxml2 rebuild py3

--- system/base/libxml2/actions.py
+++ system/base/libxml2/actions.py
@@ -7,11 +7,16 @@
 from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 
 def setup():
+    
     # fix sandbox violations when attempt to read "/missing.xml"
     pisitools.dosed("testapi.c", "\/missing.xml", "missing.xml")
-
+    
+    shelltools.system("mkdir build-py{2,3}")
+    shelltools.cd("build-py2")
+    shelltools.sym("../configure", "configure")
     options = "--with-zlib \
                --with-readline \
                --enable-ipv6 \
@@ -25,19 +30,50 @@ def setup():
         options += " --bindir=/emul32/bin \
                      --libdir=/usr/lib32 \
                      --without-python"
-    else: options += " --with-python \
+    else: options += " --with-python=/usr/bin/python \
+                       --libdir=/usr/lib"
+
+    autotools.configure(options)
+    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+    
+    shelltools.cd("../build-py3")
+    shelltools.sym("../configure", "configure")
+    options = "--with-zlib \
+               --with-readline \
+               --enable-ipv6 \
+               --includedir=/usr/include \
+               --disable-static \
+               --with-threads \
+               --with-history \
+              "
+
+    if get.buildTYPE() == "emul32":
+        options += " --bindir=/emul32/bin \
+                     --libdir=/usr/lib32 \
+                     --without-python"
+    else: options += " --with-python=/usr/bin/python3 \
                        --libdir=/usr/lib"
 
     autotools.configure(options)
     pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
 
 def build():
+    shelltools.cd("build-py2")
+    autotools.make()
+    shelltools.cd("../build-py3")
     autotools.make()
 
 def check():
+    shelltools.cd("build-py2")
+    autotools.make("check")
+    shelltools.cd("../build-py3")
     autotools.make("check")
 
 def install():
+    shelltools.cd("build-py2")
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    
+    shelltools.cd("../build-py3")
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     if get.buildTYPE() == "emul32" or "i686":
