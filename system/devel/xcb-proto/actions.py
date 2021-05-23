efc0edb26771fecd2d2dcbac973e17561e82a37e
Rmys <muscnsl@gmail.com>
2021-05-23 12:32:58 +0300

    xcb-proto rebuild

--- system/devel/xcb-proto/actions.py
+++ system/devel/xcb-proto/actions.py
@@ -4,10 +4,13 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
+    shelltools.export("PYTHON","/usr/bin/python3")
+    
     autotools.autoreconf("-vif")
     autotools.configure()
 
