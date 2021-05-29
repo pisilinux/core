cb04033e99f2c755dfb313df7ba0952e7e83ced7
Rmys <muscnsl@gmail.com>
2021-05-29 11:59:36 +0300

    libgcrypt-1.9.3 ver. bump

--- system/base/libgcrypt/actions.py
+++ system/base/libgcrypt/actions.py
@@ -6,19 +6,27 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
 def setup():
+    if get.buildTYPE() != "emul32":
+        autotools.autoreconf("-fi")
+    
     options = "--disable-static \
                --enable-noexecstack"
 
     if get.buildTYPE() == "emul32":
+        shelltools.system("sed 's:i586\*-\*-\*:x86_64-*-*:' -i mpi/config.links")
+        shelltools.system("sed 's:x86_64-\*-\*:ignore:;s:i?86-\*-\*:x86_64-*-*:' -i configure.ac")
         shelltools.export("CFLAGS", "%s -m32" % get.CFLAGS())
+        
+        autotools.autoreconf("-fi")
         options += " --libdir=/usr/lib32 --bindir=/usr/bin32"
 
         # Use 32-bit assembler, another option is to use --disable-asm option
-        pisitools.dosed("mpi/config.links", "path=\"amd64\"", "path=\"i586 i386\"")
+        #pisitools.dosed("mpi/config.links", "path=\"amd64\"", "path=\"i586 i386\"")
+        
 
     autotools.configure(options)
 
