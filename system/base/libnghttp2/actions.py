11ea83f05974e72ad48489a58804f69b36e559a2
Rmys <muscnsl@hotmail.com>
2021-05-31 19:30:30 +0300

    curl-7.77.0 ver. bump

---
+++ system/base/libnghttp2/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-i")
+    autotools.configure("--disable-hpack-tools \
+                         --disable-python-bindings \
+                         --disable-static \
+                         --with-libxml2")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
