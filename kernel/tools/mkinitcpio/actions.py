b2f35a7320e2c8efb6c25c9645c6250aa74abc0e
Berk Çakar <berk.cakar@ug.bilkent.edu.tr>
2021-09-13 23:49:52 +0300

    mkinitcpio, mkinitramfs, initrd rebuild

--- kernel/tools/mkinitcpio/actions.py
+++ kernel/tools/mkinitcpio/actions.py
@@ -8,10 +8,15 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 
-
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.removeDir("/usr/lib/kernel")
+    pisitools.removeDir("/usr/lib/tmpfiles.d")
+    pisitools.remove("/usr/lib/initcpio/install/sd-*")
+    pisitools.removeDir("/usr/share/libalpm")
+
     pisitools.dodoc("LICENSE", "README")
--- system/boot/busybox-mkinitcpio/actions.py
+++ system/boot/busybox-mkinitcpio/actions.py
@@ -6,10 +6,11 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 
 def build():
+    shelltools.export("KCONFIG_NOTIMESTAMP", "1")
     autotools.make()
-
 
 def install():
     pisitools.insinto("/usr/lib/initcpio", "busybox")
