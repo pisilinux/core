8289a359f07565b8838dc7ce13706b53dcd0ea57
Rmys <muscnsl@gmail.com>
2022-06-10 16:53:47 +0300

    wireless-regdb-2022.06.06

--- hardware/firmware/wireless-regdb/actions.py
+++ hardware/firmware/wireless-regdb/actions.py
@@ -6,12 +6,15 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
 
 def install():
-    pisitools.insinto("/usr/lib/crda/", "regulatory.bin")
-    pisitools.insinto("/lib/firmware", "regulatory.db*")
-    pisitools.insinto("/etc/wireless-regdb/pubkeys", "sforshee.key.pub.pem")
+    autotools.rawInstall("DESTDIR=%s MANDIR=/usr/share/man FIRMWARE_PATH=/lib/firmware CRDA_KEY_PATH=/etc/wireless-regdb/pubkeys" % get.installDIR())
+    #pisitools.insinto("/usr/lib/crda/", "regulatory.bin")
+    #pisitools.insinto("/lib/firmware", "regulatory.db*")
+    #pisitools.insinto("/etc/wireless-regdb/pubkeys", "sforshee.key.pub.pem")
     
-    pisitools.doman("regulatory.db.5")
+    #pisitools.doman("regulatory.db.5")
 
     pisitools.dodoc("LICENSE", "README", "CONTRIBUTING")
