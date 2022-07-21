6d62f473e7dcca5729d33615a679b9d85e214d16
Rmys <muscnsl@gmail.com>
2022-07-21 15:38:03 +0300

    polkit-0.121 ver. bump

--- system/base/polkit/actions.py
+++ system/base/polkit/actions.py
@@ -7,37 +7,28 @@
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import mesontools
 from pisi.actionsapi import get
 
 def setup():
-    #pisitools.dosed("configure.ac", "pardus-release", "pisilinux-release")
+    pisitools.dosed("meson.build", "pardus", "pisilinux")
+    pisitools.dosed("meson_options.txt", "pardus", "pisilinux")
     #pisitools.dosed("configure", "pardus-release", "pisilinux-release")
-    autotools.autoreconf("-f")
-    autotools.configure("--prefix=/usr \
-                         --sysconfdir=/etc \
-                         --localstatedir=/var \
-                         --with-pam-module-dir=/lib/security/ \
-                         --with-duktape \
-                         --with-dbus \
-                         --enable-examples \
-                         --enable-introspection \
-                         --with-systemdsystemunitdir=no \
-                         --enable-libsystemd-login=no \
-                         --enable-libelogind=yes \
-                         --disable-man-pages \
-                         --enable-gtk-doc=no \
-                         --enable-gtk-doc-html=no \
-                         --enable-gtk-doc-pdf=no \
-                         --disable-static")
-
-    pisitools.dosed("libtool", " -shared ", " -Wl,-O1,--as-needed -shared ")
+    #autotools.autoreconf("-fiv")
+    mesontools.configure("-Dos_type='pisilinux' \
+                          -Dsystemdsystemunitdir=/tmp \
+                          -Dpam_module_dir=/lib/security \
+                          -Dexamples=true \
+                          -Dauthfw='pam' \
+                          -Djs_engine='duktape' \
+                          -Dsession_tracking=libelogind")
 
 def build():
     shelltools.export('HOME', get.workDIR())
-    autotools.make()
+    mesontools.build()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s/" % get.installDIR())
+    mesontools.install()
 
     pisitools.dodir("/var/lib/polkit-1")
     shelltools.chmod("%s/var/lib/polkit-1" % get.installDIR(), mode=00700)
@@ -45,4 +36,7 @@ def install():
     shelltools.chown("%s/etc/polkit-1/rules.d" % get.installDIR(),"polkitd","root") #yada? "polkitd","root"
     shelltools.chown("%s/var/lib/polkit-1" % get.installDIR(),"polkitd","polkitd")
     shelltools.chown("%s/usr/share/polkit-1" % get.installDIR(),"polkitd","root") #yada? "polkitd","root"
-    pisitools.dodoc("AUTHORS", "NEWS", "README", "HACKING", "COPYING")
+
+    pisitools.removeDir("/tmp")
+
+    pisitools.dodoc("AUTHORS", "NEWS*", "README*", "HACKING*", "COPYING*")
