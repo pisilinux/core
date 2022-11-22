b3f037581cab5919e98e7344176343b044db3cee
Rmys <muscnsl@gmail.com>
2022-11-22 21:26:49 +0300

    libXdmcp 1.1.4

--- system/base/libXdmcp/actions.py
+++ system/base/libXdmcp/actions.py
@@ -5,32 +5,18 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    options = "--disable-static \
-               --prefix=/usr \
-               --without-xmlto"
-
-    if get.buildTYPE() == "_emul32":
-        options += " --libdir=/usr/lib32"
-
     autotools.autoreconf("-fi")
-    autotools.configure(options)
 
-
-    # autotools.configure("--disable-static \
-                         # --without-xmlto")
+    autotools.configure("--disable-static \
+                         --without-xmlto")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    if get.buildTYPE() == "_emul32":
-        return
-
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README*")
