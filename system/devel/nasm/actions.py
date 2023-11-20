dca056acc9453be03ad34e903c61b196aa14218b
Rmys <muscnsl@gmail.com>
2023-11-20 10:56:27 +0300

    nasm-2.16.01

--- system/devel/nasm/actions.py
+++ system/devel/nasm/actions.py
@@ -9,23 +9,24 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 binFiles = ["nasm", "ndisasm"]
-rdoffFiles = ["ldrdf", "rdf2bin", "rdf2ihx", "rdfdump", "rdflib", "rdx"]
+# rdoffFiles = ["ldrdf", "rdf2bin", "rdf2ihx", "rdfdump", "rdflib", "rdx"]
 
 def setup():
     autotools.configure()
 
 def build():
     autotools.make("all")
-    autotools.make("rdf")
+    # autotools.make("rdf")
 
 def install():
-    for i in rdoffFiles:
-        binFiles.append("rdoff/%s" % i)
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    # for i in rdoffFiles:
+        # binFiles.append("rdoff/%s" % i)
 
-    for i in binFiles:
-        pisitools.dobin(i)
+    # for i in binFiles:
+        # pisitools.dobin(i)
 
-    pisitools.dosym("rdf2bin", "/usr/bin/rdf2com")
+    # pisitools.dosym("rdf2bin", "/usr/bin/rdf2com")
 
     pisitools.doman("nasm.1", "ndisasm.1")
     pisitools.dodoc("AUTHORS", "CHANGES", "ChangeLog", "README*", "doc/nasmdoc.*")
