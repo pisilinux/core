ab08eb1f5ce4036aa16a5316d792346b3663c788
Rmys <muscnsl@hotmail.com>
2023-12-22 11:52:25 +0300

    libcap-ng-0.8.4

--- system/base/libcap-ng/actions.py
+++ system/base/libcap-ng/actions.py
@@ -23,4 +23,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("ChangeLog", "COPYING*", "README", "NEWS")
+    pisitools.dodoc("ChangeLog", "COPYING*", "README*", "NEWS")
