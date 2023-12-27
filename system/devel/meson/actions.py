794b2a70f9d65e4790310b6c8ff38aa11dbe4e05
Rmys <muscnsl@hotmail.com>
2023-12-27 13:14:06 +0300

    meson-1.3.1

--- system/devel/meson/actions.py
+++ system/devel/meson/actions.py
@@ -5,15 +5,16 @@
 # See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import python3modules
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def build():
-    pythonmodules.compile(pyVer="3")
+    python3modules.compile()
+
 
 def install():
-    pythonmodules.install(pyVer="3")
+    python3modules.install()
     
     pisitools.insinto("/usr/share/vim/vimfiles", "data/syntax-highlighting/vim/*")
     # pisitools.insinto("/usr/share/emacs/site-lisp", "data/syntax-highlighting/emacs/*")
