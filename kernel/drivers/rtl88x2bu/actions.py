26bf6412fcc776f658dabf37ec5aaaec67b85d32
Rmys <muscnsl@hotmail.com>
2024-07-18 11:25:24 +0300

    kernel-6.6.40

--- kernel/drivers/rtl88x2bu/actions.py
+++ kernel/drivers/rtl88x2bu/actions.py
@@ -11,7 +11,7 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import kerneltools
 
 KDIR = kerneltools.getKernelVersion()
-KVER ="6.6.36"
+KVER ="6.6.40"
 
 def build():
     autotools.make("KVER=%s" % KVER )
