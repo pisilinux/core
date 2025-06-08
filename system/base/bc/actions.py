37578dd848998b3d1459e254b4572209122afd33
mrSi-Si <ylmar714@gmail.com>
2025-06-08 14:16:40 +0300

    GNU basic calculator v1.08.2

--- system/base/bc/actions.py
+++ system/base/bc/actions.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file http://www.gnu.org/licenses/gpl.txt
+# See the file https://www.gnu.org/licenses/gpl-3.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
@@ -17,4 +17,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "FAQ", "NEWS", "README", "ChangeLog")
+    pisitools.dodoc("AUTHORS", "COPYING*", "NEWS")
