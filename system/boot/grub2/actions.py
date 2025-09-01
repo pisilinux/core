b6b95ea7d2845674d8886ad24c546a1f3202aa4e
Erkan IŞIK <erkanisik@yahoo.com>
2025-09-01 09:04:36 +0300

    grub2 theme update and core index

--- system/boot/grub2/actions.py
+++ system/boot/grub2/actions.py
@@ -75,7 +75,7 @@ def install():
     #pisitools.insinto("/boot/grub2", "unicode.pf2")
 
     # Insall our theme
-    pisitools.insinto("/usr/share/grub/themes/pisilinux","themes/Pisilinux2.4-Grub2-Themes-1.2/*")
+    pisitools.insinto("/usr/share/grub/themes/pisilinux","themes/Pisilinux2.4-Grub2-Themes-1.3/*")
 
     #remove -r 0x0-0x7F entries to fix ugly fonts or find a suitable range parameter -r ***
     shelltools.system("./grub-mkfont -o DejaVuSans-10.pf2 -r 0x0-0x7F -s 10 /usr/share/fonts/dejavu/DejaVuSans.ttf")
