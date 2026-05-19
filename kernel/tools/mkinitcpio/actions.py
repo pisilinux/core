3bebb208e936119e37b58e44270b0ac8e1178ea8
Rmys <muscnsl@gmail.com>
2026-05-19 21:11:19 +0300

    mkinitcpio

--- kernel/tools/mkinitcpio/actions.py
+++ kernel/tools/mkinitcpio/actions.py
@@ -11,6 +11,7 @@ from pisi.actionsapi import mesontools
 
 
 def setup():
+    pisitools.dosed("udev/69-dm-lvm.rules", "/usr/bin/lvm", "/usr/sbin/lvm")
     mesontools.configure("-Dsystemd=disabled")
 
 def build():
@@ -22,5 +23,11 @@ def install():
 
     pisitools.removeDir("/usr/lib/kernel")
     pisitools.removeDir("/usr/share/libalpm")
+
+    pisitools.dosed("%s/usr/bin/mkinitcpio"% get.installDIR(), "/usr/bin:/bin", "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin")
+    pisitools.dosed("%s/usr/lib/initcpio/install/udev"% get.installDIR(), "/usr/", "/")
+    pisitools.dosed("%s/usr/lib/initcpio/install/fsck"% get.installDIR(), "/usr/bin", "/sbin")
+
+    pisitools.dosed("%s/usr/bin/mkinitcpio"% get.installDIR(), "/usr/lib/modules", "/lib/modules")
 
     pisitools.dodoc("LICENSE", "README.*")
