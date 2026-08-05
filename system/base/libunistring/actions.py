b0cd9e748727cb52b41c1ef10a913b6a19b3010c
Rmys <muscnsl@gmail.com>
2026-08-05 23:00:14 +0300

    libunistring rebuild

--- system/base/libunistring/actions.py
+++ system/base/libunistring/actions.py
@@ -2,23 +2,27 @@
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
-# See the file https://www.gnu.org/licenses/gpl-3.0.txt
+# See the file http://www.gnu.org/licenses/gpl.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
 def setup():
-    autotools.autoreconf("-fiv")
-    autotools.configure("--disable-static --enable-jpeg --enable-jasper --enable-lcms")
+    autotools.autoreconf("-vfi")
+    autotools.configure("--disable-static \
+                         --disable-rpath")
+    #shelltools.system("sed -i '/pragma weak pthread_create/d' tests/glthread/thread.h")
 
 def build():
     autotools.make()
 
+#def check():
+    #autotools.make("check")
+
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    pisitools.dosym("/usr/lib/libraw.so.24.0.0", "/usr/lib/libraw.so.20")
-    pisitools.dosym("/usr/lib/libraw_r.so.24.0.0", "/usr/lib/libraw_r.so.20")
-
-    pisitools.dodoc("README*")
+    autotools.install()
+    
+    pisitools.dosym("libunistring.so.5.2.0", "/usr/lib/libunistring.so.0")
+    # pisitools.dosym("libunistring.so.5.1.0", "/usr/lib/libunistring.so.2")
+    pisitools.dodoc("AUTHORS", "BUGS", "ChangeLog", "COPYING", "COPYING.LIB", "HACKING", "NEWS", "README",  "THANKS")
